1e42666f63e42ecaf3a7c5ecddfe6121c644807c
YaraPrimeson <primesonyara@gmail.com>
2024-07-28 21:49:32 +0300

    f6

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL;
-// const API_URL = process.env.REACT_APP_API_URL ?? "https://eastcry.top/api/";
+// const API_URL = process.env.REACT_APP_API_URL;
+// const API_URL = process.env.REACT_APP_API_URL ?? "https://bitfl.onrender.com/api/";
+const API_URL = "https://bitfl.onrender.com/api/";
 const instance = axios.create({
     baseURL: API_URL,
 });
