9861dd6cec49cb4e7c3993ae6308b55ff854d130
YaraPrimeson <primesonyara@gmail.com>
2024-10-04 16:32:14 +0300

    withdraw

--- client/src/wallet/mainWallet.js
+++ client/src/wallet/mainWallet.js
@@ -100,14 +100,16 @@ const MainWallet = ({tickerData}) => {
 
                     Deposit
                 </button>
-                <NavLink to="/withdraw" className={style.btn}>
-                    <svg width="17" height="17" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M5.87521 14.1248L14.1248 5.8752M14.1248 5.8752L5.87521 5.8752M14.1248 5.8752L14.1248 14.1248"
-                            stroke="#A2F2A9" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                    </svg>
-                    Withdraw
-                </NavLink>
+                <button className={style.btn}>
+                    <NavLink to="/withdraw" className={style.btn__link}>
+                        <svg width="17" height="17" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M5.87521 14.1248L14.1248 5.8752M14.1248 5.8752L5.87521 5.8752M14.1248 5.8752L14.1248 14.1248"
+                                stroke="#A2F2A9" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                        </svg>
+                        Withdraw
+                    </NavLink>
+                </button>
                 <button className={style.btn}>
                     <svg width="17" height="17" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M16.6667 8.33334H3.33334L6.91668 4.33334M3.33334 11.6667H16.6667L13.0833 15.6667"
